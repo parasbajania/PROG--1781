ecb75ba2c27b3c8df87a90a962b03978a7341c90
parasbajania <43333769+parasbajania@users.noreply.github.com>
2018-11-13 17:51:59 +0530

    Add files via upload

---
+++ A2parasbajaniaP1/A2parasbajaniaP1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2parasbajaniaP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tution, registration, tax;
+            Console.WriteLine("The student's age");
+            string A = Console.ReadLine();
+            int B = int.Parse(A);
+            Console.WriteLine("Internatonal student enter yes or no");
+            string C = Console.ReadLine();
+            Console.WriteLine("Registration Semester(fall,summer or winter)");
+            string D = Console.ReadLine();
+
+            if (C == "not")
+            {
+                if (B <= 18)
+                {
+                    tution = 300 + (300 * 13 / 100);
+                    Console.WriteLine("Base tution $" + tution);
+                }
+                else if(B<=49)
+                {
+                    tution = 500 + (500 * 13 / 100);
+                    Console.WriteLine("Base tution $" + tution);
+                }
+                else if(B>=50)
+                {
+                    tution = 400 + (400 * 13 / 100);
+                    Console.WriteLine("Base tution $"+ tution);
+                }
+            }
+            else if (C == "yes")
+            {
+                if (B <= 18)
+                {
+                    tution = 400 + (400 * 13 / 100);
+                    Console.WriteLine("Base tution Fee $" + tution);
+                }
+                else if (B <= 49)
+                {
+                    tution = 600 + (600 * 13 / 100);
+                    Console.WriteLine("Base tution Fee $" + tution);
+                }
+                else if (B >= 50)
+                {
+                    tution = 500 + (500 * 13 / 100);
+                    Console.WriteLine("Base tution Fee $" + tution);
+                }
+            }
+            switch (D)
+            {
+                case "fall":
+                    {
+                        registration = 250 + (250 * 13 / 100);
+                        Console.WriteLine("Registration Fee for fall semester $" + registration);
+                        tax = 250 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "winter": 
+                    {
+                        registration = 220 + (220 * 13 / 100);
+                        Console.WriteLine("Registration Fee for winter semester $" + registration);
+                        tax = 220 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "summer":
+                    {
+                        registration = 150 + (150 * 13 / 100);
+                        Console.WriteLine("Registration Fee for summer semester $" + registration);
+                        tax = 150 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter a valid semester");
+                        break;
+                    }
+            }
+
+        }   
+    }
+}
---
+++ A2parasbajaniaP2/A2parasbajaniaP2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2parasbajaniaP2
+{
+    class Program
+    {      static double sphere(double r, double p)
+        {
+            double sphere = 4 / 3 * p * r * r * r;
+            return sphere;
+        }
+        static double cylinder(double r,double h, double p)
+        {
+            double cylinder = p * r * r * h;
+            return cylinder;
+        }
+        static int rectangular(int l, int b,int h)
+        {
+            int rectangular =  l * b * h;
+            return rectangular;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1:Sphere");
+            Console.WriteLine("2:Cylinder");
+            Console.WriteLine("3:Rectangular Prism");
+            string Q = Console.ReadLine();
+            int W = int.Parse(Q);
+            if (W == 1)
+            {
+                Console.WriteLine("Enter the value of radius");
+                string a = Console.ReadLine();
+                double b = double.Parse(a);
+                double result;
+                result = sphere(3.14, b);
+                Console.WriteLine("Volume of shpere is" + result);
+            }
+            else if (W == 2)
+            {
+                Console.WriteLine("Enter the value of radius");
+                string a = Console.ReadLine();
+                double b = double.Parse(a);
+                Console.WriteLine("Enter the value of height");
+                string c = Console.ReadLine();
+                double d = double.Parse(c);
+                double result;
+                result = cylinder(3.14,b,d);
+                Console.WriteLine("Volume of shpere is" + result);
+            }
+            else if (W == 3)
+            {
+                Console.WriteLine("Enter the lenght of rectangle");
+                string a = Console.ReadLine();
+                int b = int.Parse(a);
+                Console.WriteLine("Enter the breadth of rectangle");
+                string c = Console.ReadLine();
+                int d = int.Parse(c);
+                Console.WriteLine("Enter the height of rectangle");
+                string e = Console.ReadLine();
+                int f = int.Parse(e);
+                int result;
+                result = rectangular(f, b, d);
+                Console.WriteLine("Volume of shpere is" + result);
+            }
+        }
+    }
+}
